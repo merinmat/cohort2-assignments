ca477a9dc86d55033c4da3bbd348f3837d1e8fcd
Merin Anna Mathew <128686755+merinmat@users.noreply.github.com>
2024-12-21 12:16:54 +0530

    Add files via upload
    This file contains code to generate json web tokens.

---
+++ jwtgenerate.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+const jwtPassword = "secret";
+const z = require("zod");
+
+emailSchema = z.string().email();
+passwordSchema = z.string().min(6);
+
+function jwtSign(username,password){
+    const usernameCheck = emailSchema.safeParse(username);
+    const passwordCheck = passwordSchema.safeParse(password);
+
+    if(!(usernameCheck.success && passwordCheck.success)) return null;
+    const token = jwt.sign({usernameCheck}, jwtPassword)
+    return token; 
+}
+
+const ans = jwtSign("merin@gmail.com", "123456");
+console.log(ans)
+
