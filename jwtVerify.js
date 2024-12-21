f77072e127b0598d5cde79499837946b1ffe7879
Merin Anna Mathew <128686755+merinmat@users.noreply.github.com>
2024-12-21 12:43:33 +0530

    Add files via upload
    This file contains code to verify json web tokens.

---
+++ jwtVerify.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const jwtPassword = "secret";
+const z = require("zod");
+
+emailSchema = z.string().email();
+passwordSchema = z.string().min(6);
+
+function jwtSign(username,password){
+    const usernameCheck = emailSchema.safeParse(username);
+    const passwordCheck = passwordSchema.safeParse(password);
+
+    if(!(usernameCheck.success && passwordCheck.success)) return null;
+    const token = jwt.sign({usernameCheck}, jwtPassword)
+    return token; 
+}
+
+const resSign = jwtSign("merin@gmail.com", "123456");
+console.log(resSign)
+
+function jwtVerify(token){
+    const verified = jwt.verify(token, jwtPassword);
+    if(verified.usernameCheck.success) return true;
+    return false;
+}
+
+const resVerify = jwtVerify(resSign);
+console.log(resVerify)
