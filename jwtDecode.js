32dbce329e9efcc0a18b3e946dd594e5ef330eb1
Merin Anna Mathew <128686755+merinmat@users.noreply.github.com>
2024-12-21 12:42:44 +0530

    Add files via upload
    This file contains code to decode json web tokens.

---
+++ jwtDecode.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const jwtPassword = "secret";
+const z = require("zod");
+
+emailSchema = z.string().email();
+passwordSchema = z.string().min(6);
+
+function jwtSign(username,password){
+    const usernameCheck = emailSchema.safeParse(username);
+    const passwordCheck = passwordSchema.safeParse(password);
+
+    if(!(usernameCheck.success && passwordCheck.success)) return null;
+    const token = jwt.sign({usernameCheck}, jwtPassword)
+    return token; 
+}
+
+const resSign = jwtSign("merin@gmail.com", "123456");
+console.log(resSign)
+
+function jwtDecode(token){
+    const decoded = jwt.decode(token)
+    if(decoded.usernameCheck.success) return true
+    return false
+}
+
+const resDecode = jwtDecode(resSign)
+console.log(resDecode)
